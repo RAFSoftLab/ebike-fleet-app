71019489c49037f2cd1f9333b59aea0cb1fcf4fb
Aleksa Terzic <aleksa.terzic.dev@gmail.com>
2025-11-28 14:46:16 +0100

    adjust admin navigation

--- frontend/src/modules/shell/AppShell.tsx
+++ frontend/src/modules/shell/AppShell.tsx
@@ -31,6 +31,22 @@ export function AppShell({ children }: { children: React.ReactNode }) {
             <Link to="/" className="hover:underline">
               Dashboard
             </Link>
+            {isAdmin && (
+              <>
+                <Link to="/drivers" className="hover:underline">
+                  Drivers
+                </Link>
+                <Link to="/bikes" className="hover:underline">
+                  Bikes
+                </Link>
+                <Link to="/batteries" className="hover:underline">
+                  Batteries
+                </Link>
+                <Link to="/rentals" className="hover:underline">
+                  Rentals
+                </Link>
+              </>
+            )}
             <Link to="/profile" className="hover:underline">
               Profile
             </Link>
